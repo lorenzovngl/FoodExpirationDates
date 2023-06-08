e0272a82e56d27b80c64954d38033fd4d5d9359a
Lorenzo Vainigli <lorenzovngl@gmail.com>
2023-06-08 21:13:00 +0200

    🐛 Fixed potential bug

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/model/worker/CheckExpirationsWorker.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/model/worker/CheckExpirationsWorker.kt
@@ -13,6 +13,7 @@ import androidx.work.WorkerParameters
 import com.lorenzovainigli.foodexpirationdates.R
 import com.lorenzovainigli.foodexpirationdates.model.repository.ExpirationDateRepository
 import com.lorenzovainigli.foodexpirationdates.view.MainActivity
+import kotlinx.coroutines.flow.first
 import java.util.Calendar
 import javax.inject.Inject
 
@@ -25,56 +26,61 @@ class CheckExpirationsWorker @Inject constructor(
 
     override suspend fun doWork(): Result {
         val sb = StringBuilder()
-        repository.getAll().collect { list ->
-            val today = Calendar.getInstance()
-            val twoDaysAgo = Calendar.getInstance()
-            twoDaysAgo.add(Calendar.DAY_OF_MONTH, -2)
-            val yesterday = Calendar.getInstance()
-            yesterday.add(Calendar.DAY_OF_MONTH, -1)
-            val tomorrow = Calendar.getInstance()
-            tomorrow.add(Calendar.DAY_OF_MONTH, 1)
-            val msInADay = (1000 * 60 * 60 * 24)
-            val filteredList = list.filter {
-                it.expirationDate < tomorrow.time.time
-            }
-            if (filteredList.isEmpty()){
-                return@collect
-            }
-            filteredList.map {
-                sb.append(it.foodName).append(" (")
-                if (it.expirationDate < twoDaysAgo.time.time) {
-                    val days = (today.time.time - it.expirationDate) / msInADay
-                    sb.append(applicationContext.getString(R.string.n_days_ago, days))
-                } else if (it.expirationDate < yesterday.time.time)
-                    sb.append(applicationContext.getString(R.string.yesterday).lowercase())
-                else if (it.expirationDate < today.time.time){
-                    sb.append(applicationContext.getString(R.string.today).lowercase())
-                } else {
-                    sb.append(applicationContext.getString(R.string.tomorrow).lowercase())
-                }
-                sb.append("), ")
-            }
-            var message = ""
-            if (sb.toString().length > 2)
-                message = sb.toString().substring(0, sb.toString().length - 2) + "."
-            showNotification(
-                title = applicationContext.getString(R.string.your_food_is_expiring),
-                message = message
-            )
+        val today = Calendar.getInstance()
+        val twoDaysAgo = Calendar.getInstance()
+        twoDaysAgo.add(Calendar.DAY_OF_MONTH, -2)
+        val yesterday = Calendar.getInstance()
+        yesterday.add(Calendar.DAY_OF_MONTH, -1)
+        val tomorrow = Calendar.getInstance()
+        tomorrow.add(Calendar.DAY_OF_MONTH, 1)
+        val msInADay = (1000 * 60 * 60 * 24)
+        val filteredList = repository.getAll().first().filter {
+            it.expirationDate < tomorrow.time.time
         }
+        if (filteredList.isEmpty()) {
+            return Result.success()
+        }
+        filteredList.map {
+            sb.append(it.foodName).append(" (")
+            if (it.expirationDate < twoDaysAgo.time.time) {
+                val days = (today.time.time - it.expirationDate) / msInADay
+                sb.append(applicationContext.getString(R.string.n_days_ago, days))
+            } else if (it.expirationDate < yesterday.time.time)
+                sb.append(applicationContext.getString(R.string.yesterday).lowercase())
+            else if (it.expirationDate < today.time.time) {
+                sb.append(applicationContext.getString(R.string.today).lowercase())
+            } else {
+                sb.append(applicationContext.getString(R.string.tomorrow).lowercase())
+            }
+            sb.append("), ")
+        }
+        var message = ""
+        if (sb.toString().length > 2)
+            message = sb.toString().substring(0, sb.toString().length - 2) + "."
+        showNotification(
+            title = applicationContext.getString(R.string.your_food_is_expiring),
+            message = message
+        )
         return Result.success()
     }
 
     private fun showNotification(title: String, message: String) {
         if (ContextCompat.checkSelfPermission(
-            applicationContext,
-            android.Manifest.permission.POST_NOTIFICATIONS
-        ) == PackageManager.PERMISSION_GRANTED) {
-            val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE)
-                    as NotificationManager
+                applicationContext,
+                android.Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val notificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE)
+                        as NotificationManager
             val intent = Intent(applicationContext, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-            val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+            val pendingIntent = PendingIntent.getActivity(
+                applicationContext,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
             val notification = NotificationCompat.Builder(applicationContext, "channel_reminders")
                 .setContentText(message)
                 .setContentTitle(title)
