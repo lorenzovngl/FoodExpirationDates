658f90895c3047b0d4d290935ecd896b70f0cb6c
Lorenzo Vainigli <lorenzovngl@gmail.com>
2023-06-08 21:28:23 +0200

    🐛 Added missing imports

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/SettingsActivity.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/SettingsActivity.kt
@@ -20,11 +20,13 @@ import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
