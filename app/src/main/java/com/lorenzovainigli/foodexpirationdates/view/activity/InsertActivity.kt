586d32a3c51c0fedcc2a4f953794c004458f9c33
Aditya <adityakumdale@gmail.com>
2023-10-10 17:55:17 +0530

    Completed Replace Accompanist System UI Controller with Edge-to-Edge

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/activity/InsertActivity.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/activity/InsertActivity.kt
@@ -3,7 +3,9 @@ package com.lorenzovainigli.foodexpirationdates.view.activity
 import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.lorenzovainigli.foodexpirationdates.di.AppModule
 import com.lorenzovainigli.foodexpirationdates.di.DaggerAppComponent
 import com.lorenzovainigli.foodexpirationdates.view.composable.activity.InsertActivityLayout
@@ -15,6 +17,14 @@ class InsertActivity : ComponentActivity() {
     private lateinit var activity: Activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
         activity = this
         val itemId = intent.extras?.getInt("ITEM_ID")
