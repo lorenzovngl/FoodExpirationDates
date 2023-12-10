57b5b12d782d09a8c668174cc8a8c93d29449d26
Lorenzo Vainigli <lorenzovngl@gmail.com>
2023-12-10 14:37:35 +0100

    👔 Improved FoodCard

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/FoodCard.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/FoodCard.kt
@@ -1,6 +1,6 @@
 package com.lorenzovainigli.foodexpirationdates.view.composable
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -27,25 +27,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import com.lorenzovainigli.foodexpirationdates.R
 import com.lorenzovainigli.foodexpirationdates.model.entity.ExpirationDate
 import com.lorenzovainigli.foodexpirationdates.model.repository.PreferencesRepository
-import com.lorenzovainigli.foodexpirationdates.ui.theme.DarkOrange
 import com.lorenzovainigli.foodexpirationdates.ui.theme.DarkRed
-import com.lorenzovainigli.foodexpirationdates.ui.theme.DarkYellow
-import com.lorenzovainigli.foodexpirationdates.ui.theme.LightOrange
+import com.lorenzovainigli.foodexpirationdates.ui.theme.FoodExpirationDatesTheme
 import com.lorenzovainigli.foodexpirationdates.ui.theme.LightRed
-import com.lorenzovainigli.foodexpirationdates.ui.theme.LightYellow
 import com.lorenzovainigli.foodexpirationdates.ui.theme.TonalElevation
 import com.lorenzovainigli.foodexpirationdates.view.composable.screen.getItemsForPreview
-import com.lorenzovainigli.foodexpirationdates.view.preview.LanguagePreviews
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
@@ -62,42 +64,49 @@ fun FoodCard(
     val dateFormat = PreferencesRepository.getUserDateFormat(context)
     val sdf = SimpleDateFormat(dateFormat, context.resources.configuration.locales[0])
     val today = Calendar.getInstance()
-    val twoDaysAgo = Calendar.getInstance()
-    twoDaysAgo.add(Calendar.DAY_OF_MONTH, -2)
-    val yesterday = Calendar.getInstance()
-    yesterday.add(Calendar.DAY_OF_MONTH, -1)
-    val tomorrow = Calendar.getInstance()
-    tomorrow.add(Calendar.DAY_OF_MONTH, 1)
-    val withinAWeek = Calendar.getInstance()
-    withinAWeek.add(Calendar.DAY_OF_MONTH, 7)
+    val twoDaysAgo = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_MONTH, -2)
+    }
+    val yesterday = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_MONTH, -1)
+    }
+    val tomorrow = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_MONTH, 1)
+    }
+    val withinAWeek = Calendar.getInstance().apply {
+        add(Calendar.DAY_OF_MONTH, 7)
+    }
     val msInADay = (1000 * 60 * 60 * 24)
-    val expiration =
-        if (item.expirationDate < twoDaysAgo.time.time) {
-            val days = (today.time.time - item.expirationDate) / msInADay
-            stringResource(R.string.n_days_ago, days)
-        } else if (item.expirationDate < yesterday.time.time)
-            stringResource(R.string.yesterday)
-        else if (item.expirationDate < today.time.time)
-            stringResource(R.string.today)
-        else if (item.expirationDate < tomorrow.time.time)
-            stringResource(R.string.tomorrow)
-        else if (item.expirationDate < withinAWeek.time.time) {
-            val days = (item.expirationDate - today.time.time) / msInADay
-            stringResource(R.string.in_n_days, days + 1)
-        } else sdf.format(item.expirationDate)
-    val bgColor =
-        if (item.expirationDate < today.time.time)
-            if (isInDarkTheme) LightRed else DarkRed
-        else if (item.expirationDate < tomorrow.time.time)
-            if (isInDarkTheme) LightOrange else DarkOrange
-        else if (item.expirationDate < withinAWeek.time.time)
-            if (isInDarkTheme) LightYellow else DarkYellow
-        else Color.Transparent
-    val textColor =
+    val expiration = item.expirationDate.let {
+        when {
+            it < twoDaysAgo.time.time -> {
+                val days = (today.time.time - item.expirationDate) / msInADay
+                stringResource(R.string.n_days_ago, days)
+            }
+
+            it < yesterday.time.time -> stringResource(R.string.yesterday)
+            it < today.time.time -> stringResource(R.string.today)
+            it < tomorrow.time.time -> stringResource(R.string.tomorrow)
+            it < withinAWeek.time.time -> {
+                val days = (item.expirationDate - today.time.time) / msInADay
+                stringResource(R.string.in_n_days, days + 1)
+            }
+
+            else -> sdf.format(item.expirationDate)
+        }
+    }
+    val days = (item.expirationDate - today.time.time) / msInADay
+    val bgColor = item.expirationDate.let {
+        when {
+            it < today.time.time -> if (isInDarkTheme) LightRed else DarkRed
+            it < withinAWeek.time.time -> getColorForDays(days = days.toInt())
+            else -> Color.Transparent
+        }
+    }
+    val foodNameTextColor = MaterialTheme.colorScheme.onSurface
+    val daysRemainingTextColor =
         if (item.expirationDate < today.time.time) Color.White.copy(alpha = .9f)
-        else if (item.expirationDate < tomorrow.time.time) Color.Black.copy(alpha = .9f)
-        else if (item.expirationDate < withinAWeek.time.time) Color.Black.copy(alpha = .9f)
-        else MaterialTheme.colorScheme.onSurface
+        else foodNameTextColor
     Surface(
         modifier = Modifier
             .testTag(FOOD_CARD)
@@ -105,21 +114,41 @@ fun FoodCard(
             .clip(RoundedCornerShape(10.dp)),
         tonalElevation = TonalElevation.level1()
     ) {
+        val colorStops = arrayOf(
+            0f to Color.Transparent,
+            1f to bgColor
+        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(bgColor)
+                .background(
+                    Brush.linearGradient(
+                        colorStops = colorStops
+                    )
+                )
                 .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.icons8_cesto_96),
-                contentDescription = null,
-                modifier = Modifier.size(36.dp).alpha(0.8f)
-            )
+            val color = MaterialTheme.colorScheme.primary
+            val imageBitmap =
+                ImageBitmap.imageResource(id = R.drawable.icons8_cesto_96)
+            Canvas(
+                modifier = Modifier
+                    .size(36.dp)
+                    .alpha(0.8f)
+            ) {
+                drawImage(
+                    image = imageBitmap,
+                    colorFilter = ColorFilter.tint(color, BlendMode.Color)
+                )
+                drawImage(
+                    image = imageBitmap,
+                    blendMode = BlendMode.DstAtop
+                )
+            }
             Text(
                 text = item.foodName,
-                color = textColor,
+                color = foodNameTextColor,
                 modifier = Modifier
                     .weight(1f)
                     .padding(8.dp)
@@ -128,7 +157,7 @@ fun FoodCard(
             )
             Text(
                 modifier = Modifier.padding(4.dp),
-                color = textColor,
+                color = daysRemainingTextColor,
                 text = expiration
             )
             Button(
@@ -141,7 +170,7 @@ fun FoodCard(
                 onClick = onClickDelete,
                 colors = ButtonDefaults.buttonColors(
                     containerColor = MaterialTheme.colorScheme.background,
-                    contentColor = MaterialTheme.colorScheme.error
+                    contentColor = MaterialTheme.colorScheme.secondary
                 ),
                 shape = CircleShape
             ) {
@@ -154,22 +183,39 @@ fun FoodCard(
     }
 }
 
-@LanguagePreviews
+fun getColorForDays(days: Int): Color {
+    if (days > 7) {
+        return Color.Transparent
+    }
+    val maxDays = 7  // Maximum number of days (e.g., 7 days for yellow)
+    val hueEnd = 60f  // Hue for yellow
+    val hueStart = 30f  // Hue for orange
+    val fraction = days.toFloat() / maxDays
+    val hue = lerp(hueStart, hueEnd, fraction)
+    val alpha = lerp(1f, 0f, fraction)
+    val saturation = 0.8f
+    val lightness = 0.5f
+    return Color.hsl(hue = hue, saturation = saturation, lightness = lightness, alpha = alpha)
+}
+
+@PreviewLightDark
 @Composable
 fun FoodCardPreview() {
     val items = getItemsForPreview(LocalContext.current)
-    Column {
-        items.forEach {
-            FoodCard(
-                item = ExpirationDate(
-                    id = 0,
-                    foodName = it.foodName,
-                    expirationDate = it.expirationDate
-                ),
-                onClickEdit = {},
-                onClickDelete = {},
-                isInDarkTheme = isSystemInDarkTheme()
-            )
+    FoodExpirationDatesTheme {
+        Column {
+            items.forEach {
+                FoodCard(
+                    item = ExpirationDate(
+                        id = 0,
+                        foodName = it.foodName,
+                        expirationDate = it.expirationDate
+                    ),
+                    onClickEdit = {},
+                    onClickDelete = {},
+                    isInDarkTheme = isSystemInDarkTheme()
+                )
+            }
         }
     }
 }
