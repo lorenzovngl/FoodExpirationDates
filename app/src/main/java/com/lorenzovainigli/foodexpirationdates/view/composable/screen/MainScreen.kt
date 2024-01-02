9763fd65d0dbed19b83e252564262d1171bc2ebb
Lorenzo Vainigli <lorenzovngl@gmail.com>
2024-01-02 18:44:30 +0100

    🎨 Enhanced color contrast

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/screen/MainScreen.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/screen/MainScreen.kt
@@ -22,11 +22,11 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -118,7 +118,7 @@ fun ListOfItems(
 }
 
 @Composable
-@Preview
+@PreviewLightDark
 fun ListOfItemsPreview() {
     FoodExpirationDatesTheme {
         Surface {
@@ -146,7 +146,7 @@ fun EmptyList() {
             Text(
                 text = stringResource(id = R.string.no_items_found),
                 style = MaterialTheme.typography.displaySmall,
-                color = Color.Gray.copy(alpha = 0.5f),
+                color = MaterialTheme.colorScheme.secondary.copy(alpha = 0.9f),
                 textAlign = TextAlign.Center
             )
             Text(
@@ -158,7 +158,7 @@ fun EmptyList() {
 }
 
 @Composable
-@Preview
+@PreviewLightDark
 fun EmptyListPreview() {
     FoodExpirationDatesTheme {
         Surface {
