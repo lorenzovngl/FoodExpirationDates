1c6b17762eeb02b1d82651dc0ec6763b67f2670b
Lorenzo Vainigli <lorenzovngl@gmail.com>
2024-03-10 12:40:05 +0100

    🔧 Disabled dynamic colors by default

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/ui/theme/Theme.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/ui/theme/Theme.kt
@@ -75,7 +75,7 @@ private val DarkColors = darkColorScheme(
 fun FoodExpirationDatesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
