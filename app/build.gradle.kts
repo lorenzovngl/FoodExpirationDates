724f4d3fdb50094f38e14db732b0f28c69bd3c44
Lorenzo Vainigli <lorenzovngl@gmail.com>
2024-05-05 16:18:44 +0200

    🔖 Version 2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.lorenzovainigli.foodexpirationdates"
         minSdk = 24
         targetSdk = 34
-        versionCode = 26
-        versionName = "2.2.0"
+        versionCode = 27
+        versionName = "2.3.0"
 
         archivesName.set("FoodExpirationDates-$versionName")
 
