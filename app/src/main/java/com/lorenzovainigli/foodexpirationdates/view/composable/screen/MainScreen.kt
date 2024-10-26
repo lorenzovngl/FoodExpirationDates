df7c25a42a9c9d5a7942feaf038603b3679b038b
Lorenzo Vainigli <lorenzovngl@gmail.com>
2024-10-26 22:53:23 +0200

    🐛 Add a space to avoid hiding the last foodcard

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/screen/MainScreen.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/screen/MainScreen.kt
@@ -6,7 +6,9 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -115,6 +117,7 @@ fun ListOfItems(
                 }
             )
         }
+        Spacer(modifier = Modifier.height(70.dp))
     }
 }
 
