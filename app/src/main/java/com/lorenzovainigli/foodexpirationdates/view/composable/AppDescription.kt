66af8f310e872b4b7268acfe118e3f4b3b40724c
Lorenzo Vainigli <lorenzovngl@gmail.com>
2025-10-02 17:46:56 +0200

    📝 Add link to developer's website in the app description
    This commit enhances the app description by making the developer's name a clickable link that opens their website.
    
    This was achieved by:
    - Creating a new `AppDescription` composable that uses an `AnnotatedString` to add a link to the developer's name.
    - Adding constants for the developer's name and website URL in `Utils.kt`.
    - Replacing the simple `Text` in `InfoScreen.kt` with the new `AppDescription` composable.

---
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/view/composable/AppDescription.kt
@@ -0,0 +1,74 @@
+package com.lorenzovainigli.foodexpirationdates.view.composable
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
+import com.lorenzovainigli.foodexpirationdates.DEVELOPER_NAME
+import com.lorenzovainigli.foodexpirationdates.DEVELOPER_WEBSITE
+import com.lorenzovainigli.foodexpirationdates.R
+import com.lorenzovainigli.foodexpirationdates.ui.theme.FoodExpirationDatesTheme
+
+const val URL_TAG = "URL"
+
+@Composable
+fun AppDescription(
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.bodyLarge
+) {
+    val uriHandler = LocalUriHandler.current
+    val fullText = stringResource(R.string.app_description, stringResource(id = R.string.app_name))
+
+    val start = fullText.indexOf(DEVELOPER_NAME)
+    val end = start + DEVELOPER_NAME.length
+
+    val annotatedText = buildAnnotatedString {
+        append(fullText)
+
+        if (start >= 0) {
+            addStyle(
+                style = SpanStyle(
+                    color = MaterialTheme.colorScheme.primary
+                ),
+                start = start,
+                end = end
+            )
+            addStringAnnotation(
+                tag = URL_TAG,
+                annotation = DEVELOPER_WEBSITE,
+                start = start,
+                end = end
+            )
+        }
+    }
+
+    BasicText(
+        modifier = modifier.pointerInput(Unit){
+            detectTapGestures(
+                onTap = { offset ->
+                    annotatedText.getStringAnnotations(URL_TAG, start, end)
+                        .firstOrNull()
+                        ?.let { uriHandler.openUri(it.item) }
+                }
+            )
+        },
+        text = annotatedText,
+        style = textStyle
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AboutThisAppIntroPreview() {
+    FoodExpirationDatesTheme {
+        AppDescription()
+    }
+}
