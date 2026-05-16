526ac937bc12d7fc473d3c9e83f220dbed208a0f
Lorenzo Vainigli <lorenzovngl@gmail.com>
2026-05-16 15:00:37 +0200

    ♻️ Update `LocalLifecycleOwner` import

--- app/src/full/java/com/lorenzovainigli/foodexpirationdates/view/composable/CameraPreview.kt
+++ app/src/full/java/com/lorenzovainigli/foodexpirationdates/view/composable/CameraPreview.kt
@@ -8,9 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import com.lorenzovainigli.foodexpirationdates.ui.theme.FoodExpirationDatesTheme
 
 @Composable
