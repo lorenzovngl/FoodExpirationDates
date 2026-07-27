366d121ee85de6eb5053f8ef7c483826c6329b11
Lorenzo Vainigli <lorenzovngl@gmail.com>
2026-07-27 21:46:39 +0200

    🐛 Add support for RefreshNewsWorker in MyWorkerFactory

--- app/src/main/java/com/lorenzovainigli/foodexpirationdates/model/worker/MyWorkerFactory.kt
+++ app/src/main/java/com/lorenzovainigli/foodexpirationdates/model/worker/MyWorkerFactory.kt
@@ -5,20 +5,37 @@ import androidx.work.ListenableWorker
 import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
 import com.lorenzovainigli.foodexpirationdates.model.repository.ExpirationDateRepository
+import com.lorenzovainigli.news.data.worker.RefreshNewsWorker
+import com.lorenzovainigli.news.domain.usecase.RefreshNewsUseCase
 import java.time.Clock
 import javax.inject.Inject
 
 class MyWorkerFactory @Inject constructor(
-    private val repository: ExpirationDateRepository
+    private val repository: ExpirationDateRepository,
+    private val refreshNewsUseCase: RefreshNewsUseCase
 ) : WorkerFactory() {
     override fun createWorker(
         appContext: Context,
         workerClassName: String,
         workerParameters: WorkerParameters
-    ): ListenableWorker = CheckExpirationsWorker(
-        appContext = appContext,
-        params = workerParameters,
-        repository = repository,
-        clock = Clock.systemDefaultZone()
-    )
+    ): ListenableWorker? {
+        return when (workerClassName) {
+            CheckExpirationsWorker::class.java.name -> {
+                CheckExpirationsWorker(
+                    appContext = appContext,
+                    params = workerParameters,
+                    repository = repository,
+                    clock = Clock.systemDefaultZone()
+                )
+            }
+            RefreshNewsWorker::class.java.name -> {
+                RefreshNewsWorker(
+                    appContext = appContext,
+                    params = workerParameters,
+                    refreshNewsUseCase = refreshNewsUseCase,
+                )
+            }
+            else -> null
+        }
+    }
 }
