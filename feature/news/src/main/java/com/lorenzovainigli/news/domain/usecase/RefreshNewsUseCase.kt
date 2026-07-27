fd2ee65c33f46a4ff48e92145be48cb4091f84b8
Lorenzo Vainigli <lorenzovngl@gmail.com>
2026-07-27 21:22:07 +0200

    🐛 Add missing update of last refresh timestamp in RefreshNewsUseCase

--- feature/news/src/main/java/com/lorenzovainigli/news/domain/usecase/RefreshNewsUseCase.kt
+++ feature/news/src/main/java/com/lorenzovainigli/news/domain/usecase/RefreshNewsUseCase.kt
@@ -1,12 +1,16 @@
 package com.lorenzovainigli.news.domain.usecase
 
+import com.lorenzovainigli.news.domain.repository.NewsPreferencesRepository
 import com.lorenzovainigli.news.domain.repository.NewsRepository
 import javax.inject.Inject
 
 class RefreshNewsUseCase @Inject constructor(
-    private val newsRepository: NewsRepository
+    private val newsRepository: NewsRepository,
+    private val preferencesRepository: NewsPreferencesRepository,
 ) {
     suspend operator fun invoke(): Result<Unit> {
+        val now = System.currentTimeMillis()
+        preferencesRepository.setLastRefreshTimestamp(now)
         return newsRepository.refreshNews()
     }
 }
